ae8a317fbb33ef3a2de8581ad72ffc652a1b5166
Logan Snopek <LSnopek@lcs.on.ca>
2021-11-30 14:26:38 -0500

    Initial Commit

---
+++ GessingGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GessingGame
+//
+//  Created by Logan Snopek on 2021-11-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GessingGame/GessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GessingGameApp.swift
+//  GessingGame
+//
+//  Created by Logan Snopek on 2021-11-30.
+//
+
+import SwiftUI
+
+@main
+struct GessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
